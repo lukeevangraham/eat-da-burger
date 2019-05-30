0b369fe6e911d5573f783acf833bf098a36eff82
Cefalu Wave <luke.evan.graham@gmail.com>
2019-05-29 22:12:08 -0700

    database and config files setup

---
+++ config/connection.js
@@ -0,0 +1,17 @@
+var mysql = require("mysql");
+
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 8889,
+    user: "root",
+    password: "root",
+    database: "burgers_db"
+  });
+  
+  // connect to the mysql server and sql database
+  connection.connect(function(err) {
+    if (err) throw err;
+  });
+
+  module.exports = connection;
---
+++ config/orm.js
@@ -0,0 +1,46 @@
+var db = require('./connection');
+
+// Object Relational Mapper (ORM)
+
+// The ?? signs are for swapping out table or column names
+// The ? signs are for swapping out other values
+// These help avoid SQL injection
+// https://en.wikipedia.org/wiki/SQL_injection
+
+// CREATE SELECTALL METHOD
+
+// CREATE INSERTONE METHOD
+
+// CREATE UPDATEONE METHOD
+
+var orm = {
+    selectAll: function (whatToSelect, tableInput) {
+        var queryString = "SELECT * FROM burgers";
+        connection.query(queryString, function (err, result) {
+            if (err) throw err;
+            console.log(result);
+        });
+    },
+    insertOne: function (burgerName) {
+
+        connection.query("INSERT INTO burgers SET ?",
+            {
+                burger_name: burgerName,
+                devoured: FALSE
+            },
+            function (err, result) {
+                if (err) throw err;
+                console.log(result);
+            });
+    },
+    updateOne: function (burgerNumber) {
+
+        connection.query(`UPDATE burgers SET devoured = TRUE WHERE item_id = ?`, [burgerNumber],
+            function (err, result) {
+                if (err) throw err;
+                console.log(result);
+            });
+    }
+};
+
+module.exports = orm;
