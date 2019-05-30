ffe08b14e6873aea084a443fa260f16bf010db8c
Cefalu Wave <luke.evan.graham@gmail.com>
2019-05-29 22:58:02 -0700

    setting up calls to the ORM functions

--- models/burger.js
+++ models/burger.js
@@ -1,2 +1,21 @@
 var orm = require("../config/orm")
 
+var burger = {
+    selectAll: function (cb) {
+        orm.selectAll(function(res) {
+            cb(res);
+        });
+    },
+    insertOne: function(burnerName, cb) {
+        orm.insertOne(burgerName, function(res) {
+            cb(res);
+        });
+    },
+    updateOne: function(burgerNumber, cb) {
+        orm.updateOne(burgerNumber, function(res) {
+            cb(res);
+        });
+    }
+}
+
+module.exports = burger;
