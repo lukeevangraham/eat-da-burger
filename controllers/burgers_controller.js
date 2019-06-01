f0b50ad3d11fae314da087a102e2013c4684cbe0
Cefalu Wave <luke.evan.graham@gmail.com>
2019-06-01 09:07:53 -0700

    index displays some information for db

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -9,7 +9,7 @@ var burger = require("../models/burger");
 router.get("/", function(req, res) {
   burger.selectAll(function(data) {
     var hbsObject = {
-      burger: data
+      burgers: data
     };
     console.log(hbsObject);
     res.render("index", hbsObject);
